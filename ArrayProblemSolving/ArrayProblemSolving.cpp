64cecc3c3ca698d7165ce18415c311d2320b3dd4
kksmash123 <kksmashes@gmail.com>
2022-08-10 19:44:32 +0530

    missing element method 1 by formula

--- ArrayProblemSolving/ArrayProblemSolving.cpp
+++ ArrayProblemSolving/ArrayProblemSolving.cpp
@@ -1,8 +1,40 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
+void MissingElementInSequenceM1()
+{
+	vector<int> A = { 1,2,3,4,6,7,8,9,10,11,12 };
+
+	int SumOfNumbers = 0, SumInArray = 0, MissingElement;
+
+	int lastElement = A.back();
+
+	// sum of n natural numbers , n-> last element
+
+	SumOfNumbers = (lastElement * (lastElement + 1)) / 2;
+
+	cout << "Sum of " << lastElement << " Consequtive Number = " << SumOfNumbers << endl;
+
+	// sum of elements in the Array
+
+	for (int i = 0;i < A.size();i++)
+		SumInArray += A[i];
+
+	cout << "Sum of elements in the Array = " << SumInArray << endl;
+
+	// finding the missing element 
+
+	MissingElement = SumOfNumbers - SumInArray;
+
+	cout << "Missing element in the Array is : " << MissingElement << endl;
+
+}
+
+
 int main()
 {
+	MissingElementInSequenceM1();
 	return 0;
 }
